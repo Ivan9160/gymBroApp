5c4144e6ee0e9f11fd5d337a19fa38bacbbaa05e
Ivan <ivanjsdev2006@gmail.com>
2026-08-10 17:46:19 +0200

    fix: increase novice stress multiplier from 1.4 to 2.3 so stress is increased for novices

--- server/src/resources/businessLogic/coreMath.config.ts
+++ server/src/resources/businessLogic/coreMath.config.ts
@@ -1,5 +1,5 @@
 export const CoreMathConfig = {
     INTENSITY_POWER: 2.5, // p>1: Higher intensity sets have a disproportionately larger impact on proficiency. p<1: Lower intensity sets have a disproportionately larger impact on proficiency.
-    MUSCLE_FACTOR_SOFTNESS: 1.5, // Softness factor for muscle factor influence on proficiency. Higher values make proficiency less sensitive to muscle factors.
+    MUSCLE_FACTOR_SOFTNESS: 1.7, // Softness factor for muscle factor influence on proficiency. Higher values make proficiency less sensitive to muscle factors.
     DAMAGE_RISE_HOURS: 10, // Time in hours for the damage to rise to its peak after a workout
 }
--- server/src/resources/businessLogic/soreness/soreness.config.ts
+++ server/src/resources/businessLogic/soreness/soreness.config.ts
@@ -5,6 +5,6 @@ export const SorenessConfig = {
   MIN_RECOVERY_REDUCTION: 0.3, // Minimum reduction in recovery time due to proficiency, ensuring that even highly proficient users have a baseline recovery period
   REPEATED_BOUT_LOOKBACK_DAYS: 21,       // window of time to look back for repeated bouts of exercise for the same muscle group
   REPEATED_BOUT_SATURATION_SESSIONS: 3,  // amount of sessions within the lookback window after which the repeated bout effect is considered saturated
-  NOVICE_STRESS_MULTIPLIER: 1.4,         // multiplier for novice/infrequently trained groups
+  NOVICE_STRESS_MULTIPLIER: 2.3,         // multiplier for novice/infrequently trained groups
   ADAPTED_STRESS_MULTIPLIER: 0.7,        // multiplier for regularly trained groups
 };
